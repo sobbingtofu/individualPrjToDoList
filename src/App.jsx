c1b99f128c079b0710be2f4d37b67cec404a96e3
sobbingtofu <kukubasak@naver.com>
2024-05-16 05:55:20 +0900

    fix: 진행중/완료 리스트 오고가는 로직 불린형으로 변경해 간소화

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,49 @@
 import { useState } from 'react'
 import './App.css'
 
- 
-// id: 0, title: "가제", content: "내용", isDone: false
-
-
 const App = () => {
   const [todoList, setTodoList] = useState([]);
-  const [doneList, setDoneList] = useState([]);
-  
   const [inputTitle, setInputTitle] = useState("");
   const [inputContent, setInputContent] = useState("");
 
-
   const addTodo = () => {
     const newTodo = {
-      id: toString(new Date()),
+      id: todoList.length+1,
       title: inputTitle,
       content: inputContent,
+      isDone: false
     };
-
     setTodoList([...todoList, newTodo])
-
   }
 
   const deleteTodo = (e) => {
-    
     todoList.forEach((todo, index) => {
-      if(todo.id === toString(e.target.id)) {
+      if(todo.id === parseInt(e.target.id)) {
         todoList.splice(index,1);
       }
     })
     setTodoList([...todoList]);
   }
 
-  const moveToDoneList = (e) => {
+  const setToDone = (e) => {
     console.log(e.target.id);
-    todoList.forEach((todo, index) => {
-      if(todo.id === toString(e.target.id)) {
-        doneList.push(todo);
-        todoList.splice(index,1);
+    todoList.forEach((todo) => {
+      if(todo.id === parseInt(e.target.id)) {
+        todo.isDone = true;
       }
     })
-    setDoneList([...doneList]);
     setTodoList([...todoList]);
   }
 
-  const deleteDone = (e) => {
-    doneList.forEach((done, index) => {
-      if(done.id === toString(e.target.id)) {
-        doneList.splice(index,1);
-      }
-    })
-    setTodoList([...doneList]);
-  }
-
-  const moveToTodoList = (e) => {
+  const setToUndone = (e) => {
     console.log(e.target.id);
-    doneList.forEach((done, index) => {
-      if(done.id === toString(e.target.id)) {
-        todoList.push(done);
-        doneList.splice(index,1);
+    todoList.forEach((todo) => {
+      if(todo.id === parseInt(e.target.id)) {
+        todo.isDone = false;
       }
     })
-    setDoneList([...doneList]);
     setTodoList([...todoList]);
   }
-
 
   return (
     <>
@@ -77,52 +54,48 @@ const App = () => {
           <p className='input-label-title'>제목</p>
           <input className='input-title' id='input-title' value = {inputTitle}
             onChange={(e) => {setInputTitle(e.target.value);}}/>
-
           <p className='input-label-content'>내용</p>
           <input className='input-content' id='input-content' value = {inputContent}
             onChange={(e) => {setInputContent(e.target.value)}}/>
-
         </section>
         <button className='add-button' id='add-button' onClick={addTodo}>추가하기</button>
       </section>
       <h3>Working</h3>
       <section className='todo-list-section'>
         {todoList.map((todo) => {
-          return (
-          // eslint-disable-next-line react/jsx-key
-          <section className='todo-section' id={todo.id}>
-            <section className='todo-text-section'>
-              <p className='todo-title'>{todo.title}</p>
-              <p className='todo-content'>{todo.content}</p>
-            </section>
-            <section className='todo-btn-section'>
-              <button className='delete-btn' id={todo.id} onClick={deleteTodo}>삭제하기</button>
-              <button className='complete-btn' id={todo.id} onClick={moveToDoneList}>완료</button>
-            </section>
-          </section>
-          
-          );
-        })}
+          if(todo.isDone === false) {
+            return (
+              // eslint-disable-next-line react/jsx-key
+              <section className='todo-section' id={todo.id}>
+                <section className='todo-text-section'>
+                  <p className='todo-title'>{todo.title}</p>
+                  <p className='todo-content'>{todo.content}</p>
+                </section>
+                <section className='todo-btn-section'>
+                  <button className='delete-btn' id={todo.id} onClick={deleteTodo}>삭제하기</button>
+                  <button className='complete-btn' id={todo.id} onClick={setToDone}>완료</button>
+                </section>
+              </section>
+              );
+          }})}
       </section>
       <h3>Done</h3>
       <section className='done-list-section'>
-        {doneList.map((doneItem) => {
-          
-          return (
-          // eslint-disable-next-line react/jsx-key
-          <section className='todo-section'>
-
-            <section className='todo-text-section'>
-              <p className='done-title'>{doneItem.title}</p>
-              <p className='done-content'>{doneItem.content}</p>
+        {todoList.map((todo) => {
+           if(todo.isDone === true) {
+            return (
+            // eslint-disable-next-line react/jsx-key
+            <section className='todo-section'>
+              <section className='todo-text-section'>
+                <p className='done-title'>{todo.title}</p>
+                <p className='done-content'>{todo.content}</p>
+              </section>
+              <section className='todo-btn-section'>
+                <button className='delete-btn' id={todo.id} onClick={deleteTodo}>삭제하기</button>
+                <button className='complete-btn'id={todo.id} onClick={setToUndone}>취소</button>
+              </section>
             </section>
-            <section className='todo-btn-section'>
-              <button className='delete-btn' id={doneItem.id} onClick={deleteDone}>삭제하기</button>
-              <button className='complete-btn'id={doneItem.id} onClick={moveToTodoList}>취소</button>
-            </section>
-          </section>
-          
-          );
+            );}
         })}
       </section>
     </>
