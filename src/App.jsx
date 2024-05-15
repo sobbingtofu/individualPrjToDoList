5b0dffd98eccdd43006c87ce7ead2a09bd8b87c4
sobbingtofu <kukubasak@naver.com>
2024-05-16 05:46:07 +0900

    update: 1차적 기능 구현 완료... (리팩토링 매우 많이 필요)

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,130 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+ 
+// id: 0, title: "가제", content: "내용", isDone: false
+
+
+const App = () => {
+  const [todoList, setTodoList] = useState([]);
+  const [doneList, setDoneList] = useState([]);
+  
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputContent, setInputContent] = useState("");
+
+
+  const addTodo = () => {
+    const newTodo = {
+      id: toString(new Date()),
+      title: inputTitle,
+      content: inputContent,
+    };
+
+    setTodoList([...todoList, newTodo])
+
+  }
+
+  const deleteTodo = (e) => {
+    
+    todoList.forEach((todo, index) => {
+      if(todo.id === toString(e.target.id)) {
+        todoList.splice(index,1);
+      }
+    })
+    setTodoList([...todoList]);
+  }
+
+  const moveToDoneList = (e) => {
+    console.log(e.target.id);
+    todoList.forEach((todo, index) => {
+      if(todo.id === toString(e.target.id)) {
+        doneList.push(todo);
+        todoList.splice(index,1);
+      }
+    })
+    setDoneList([...doneList]);
+    setTodoList([...todoList]);
+  }
+
+  const deleteDone = (e) => {
+    doneList.forEach((done, index) => {
+      if(done.id === toString(e.target.id)) {
+        doneList.splice(index,1);
+      }
+    })
+    setTodoList([...doneList]);
+  }
+
+  const moveToTodoList = (e) => {
+    console.log(e.target.id);
+    doneList.forEach((done, index) => {
+      if(done.id === toString(e.target.id)) {
+        todoList.push(done);
+        doneList.splice(index,1);
+      }
+    })
+    setDoneList([...doneList]);
+    setTodoList([...todoList]);
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>My Todo List</h1>
+    <h3>추가하기</h3>
+      <section className='add-section'>
+        <section className='inputs'>        
+          <p className='input-label-title'>제목</p>
+          <input className='input-title' id='input-title' value = {inputTitle}
+            onChange={(e) => {setInputTitle(e.target.value);}}/>
+
+          <p className='input-label-content'>내용</p>
+          <input className='input-content' id='input-content' value = {inputContent}
+            onChange={(e) => {setInputContent(e.target.value)}}/>
+
+        </section>
+        <button className='add-button' id='add-button' onClick={addTodo}>추가하기</button>
+      </section>
+      <h3>Working</h3>
+      <section className='todo-list-section'>
+        {todoList.map((todo) => {
+          return (
+          // eslint-disable-next-line react/jsx-key
+          <section className='todo-section' id={todo.id}>
+            <section className='todo-text-section'>
+              <p className='todo-title'>{todo.title}</p>
+              <p className='todo-content'>{todo.content}</p>
+            </section>
+            <section className='todo-btn-section'>
+              <button className='delete-btn' id={todo.id} onClick={deleteTodo}>삭제하기</button>
+              <button className='complete-btn' id={todo.id} onClick={moveToDoneList}>완료</button>
+            </section>
+          </section>
+          
+          );
+        })}
+      </section>
+      <h3>Done</h3>
+      <section className='done-list-section'>
+        {doneList.map((doneItem) => {
+          
+          return (
+          // eslint-disable-next-line react/jsx-key
+          <section className='todo-section'>
+
+            <section className='todo-text-section'>
+              <p className='done-title'>{doneItem.title}</p>
+              <p className='done-content'>{doneItem.content}</p>
+            </section>
+            <section className='todo-btn-section'>
+              <button className='delete-btn' id={doneItem.id} onClick={deleteDone}>삭제하기</button>
+              <button className='complete-btn'id={doneItem.id} onClick={moveToTodoList}>취소</button>
+            </section>
+          </section>
+          
+          );
+        })}
+      </section>
     </>
   )
 }
