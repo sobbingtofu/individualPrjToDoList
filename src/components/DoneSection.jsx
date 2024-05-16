0c4d1ba76b0870f4c0575c3ba6276346d6450eff
sobbingtofu <kukubasak@naver.com>
2024-05-16 14:32:39 +0900

    update: 로컬스토리지와 연동 완료

--- src/components/DoneSection.jsx
+++ src/components/DoneSection.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
 import "./style/DoneSection.css";
 import "../App.css";
@@ -5,20 +6,26 @@ import "../App.css";
 const DoneSection = ({todoList, setTodoList}) => {
   const deleteTodo = (e) => {
     const deletedTodoList = todoList.filter((todo) => {
-      return parseInt(todo.id) !== parseInt(e.target.id);
+      return parseInt(todo.id) !== parseInt(e.target.parentElement.parentElement.id);
     });
     setTodoList(deletedTodoList);
+
+    // 로컬스토리지에도 삭제 적용된 deletedTodoList를 반영시킴
+    localStorage.setItem("todos", JSON.stringify(deletedTodoList));
   };
 
   // 취소 버튼 누르면 실행될 함수
   // 버튼 눌린 대상과 id가 같은 todo 객체의 isDone 속성을 false 바꿔 Working 섹션에 표기되도록 함
   const shiftToTodo = (e) => {
     todoList.forEach((todo) => {
-      if (todo.id === parseInt(e.target.id)) {
+      if (todo.id === parseInt(e.target.parentElement.parentElement.id)) {
         todo.isDone = false;
       }
     });
     setTodoList([...todoList]);
+
+    // 로컬스토리지에도 todo의 이동 적용된 deletedTodoList를 반영시킴
+    localStorage.setItem("todos", JSON.stringify([...todoList]));
   };
 
   return (
@@ -28,17 +35,16 @@ const DoneSection = ({todoList, setTodoList}) => {
         {todoList.map((todo) => {
           if (todo.isDone === true) {
             return (
-              // eslint-disable-next-line react/jsx-key
-              <section className="todo-section">
+              <section className="todo-section" id={todo.id}>
                 <section className="todo-text-section">
                   <p className="done-title">{todo.title}</p>
                   <p className="done-content">{todo.content}</p>
                 </section>
                 <section className="todo-btn-section">
-                  <button className="delete-btn" id={todo.id} onClick={deleteTodo}>
+                  <button className="delete-btn" onClick={deleteTodo}>
                     삭제하기
                   </button>
-                  <button className="cancel-btn" id={todo.id} onClick={shiftToTodo}>
+                  <button className="cancel-btn" onClick={shiftToTodo}>
                     취소
                   </button>
                 </section>
--- src/components/WorkingSection.jsx
+++ src/components/WorkingSection.jsx
@@ -8,19 +8,24 @@ const WorkingSection = ({todoList, setTodoList}) => {
   // 버튼 눌린 대상과 id가 같은 todo를 todoList에서 삭제한 deletedTodoList를 TodoList로 재설정해줌
   const deleteTodo = (e) => {
     const deletedTodoList = todoList.filter((todo) => {
-      return parseInt(todo.id) !== parseInt(e.target.id);
+      return parseInt(todo.id) !== parseInt(e.target.parentElement.parentElement.id);
     });
     setTodoList(deletedTodoList);
+    // 로컬스토리지에도 삭제 적용된 deletedTodoList를 반영시킴
+    localStorage.setItem("todos", JSON.stringify(deletedTodoList));
   };
   // 완료 버튼 누르면 실행될 함수
   // 버튼 눌린 대상과 id가 같은 todo 객체의 isDone 속성을 true로 바꿔 Done 섹션에 표기되도록 함
   const shiftToDone = (e) => {
     todoList.forEach((todo) => {
-      if (todo.id === parseInt(e.target.id)) {
+      if (todo.id === parseInt(e.target.parentElement.parentElement.id)) {
         todo.isDone = true;
       }
     });
     setTodoList([...todoList]);
+
+    // 로컬스토리지에도 todo의 이동 적용된 deletedTodoList를 반영시킴
+    localStorage.setItem("todos", JSON.stringify([...todoList]));
   };
 
   return (
@@ -37,10 +42,10 @@ const WorkingSection = ({todoList, setTodoList}) => {
                   <p className="todo-content">{todo.content}</p>
                 </section>
                 <section className="todo-btn-section">
-                  <button className="delete-btn" id={todo.id} onClick={deleteTodo}>
+                  <button className="delete-btn" onClick={deleteTodo}>
                     삭제하기
                   </button>
-                  <button className="complete-btn" id={todo.id} onClick={shiftToDone}>
+                  <button className="complete-btn" onClick={shiftToDone}>
                     완료
                   </button>
                 </section>
