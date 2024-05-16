ed3174db5e3a99d082d4747d68fe80bb209a2f56
sobbingtofu <kukubasak@naver.com>
2024-05-16 12:46:11 +0900

    update: 각주 추가 및 id 로직 개선, 리팩토링 준비 완료

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,35 @@ import { useState } from 'react'
 import './App.css'
 
 const App = () => {
+  // todo 객체들이 저장되는 배열 todoList 생성
   const [todoList, setTodoList] = useState([]);
+
+  // 사용자가 input하는 정보가 담기는 state들 생성
   const [inputTitle, setInputTitle] = useState("");
   const [inputContent, setInputContent] = useState("");
 
+  // 추가하기 버튼 누르면 실행될 함수
+  // 사용자가 input한 정보를 새로운 todo 객체로 만들고, 이를 todoList 배열에 저장
   const addTodo = () => {
     const newTodo = {
-      id: todoList.length+1,
+      // id: todoList.length+1, 중복 이슈 발생
+      id: Date.now(),
       title: inputTitle,
       content: inputContent,
       isDone: false
     };
     setTodoList([...todoList, newTodo])
   }
-
+  // 삭제하기 버튼 누르면 실행될 함수
+  // 버튼 눌린 대상과 id가 같은 todo를 todoList에서 삭제한 deletedTodoList를 TodoList로 재설정해줌
   const deleteTodo = (e) => {
-    todoList.forEach((todo, index) => {
-      if(todo.id === parseInt(e.target.id)) {
-        todoList.splice(index,1);
-      }
+    const deletedTodoList = todoList.filter((todo) => {
+      return (parseInt(todo.id) !== parseInt(e.target.id));
     })
-    setTodoList([...todoList]);
+    setTodoList(deletedTodoList);
   }
-
+  // 완료 버튼 누르면 실행될 함수
+  // 버튼 눌린 대상과 id가 같은 todo 객체의 isDone 속성을 true로 바꿔 Done 섹션에 표기되도록 함
   const setToDone = (e) => {
     console.log(e.target.id);
     todoList.forEach((todo) => {
@@ -35,6 +41,8 @@ const App = () => {
     setTodoList([...todoList]);
   }
 
+  // 취소 버튼 누르면 실행될 함수
+  // 버튼 눌린 대상과 id가 같은 todo 객체의 isDone 속성을 false 바꿔 Working 섹션에 표기되도록 함
   const setToUndone = (e) => {
     console.log(e.target.id);
     todoList.forEach((todo) => {
