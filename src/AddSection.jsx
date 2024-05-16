b641b5363bca2793df832a5d3ccd7fbacc2fa001
sobbingtofu <kukubasak@naver.com>
2024-05-16 13:34:31 +0900

    fix: prettier가 디폴트로 적용되지 않던 이슈 해결

--- src/AddSection.jsx
+++ src/AddSection.jsx
@@ -1,42 +1,55 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react'
-import './App.css'
+import {useState} from "react";
+import "./App.css";
 
 const AddSection = ({todoList, setTodoList}) => {
-    // 사용자가 input하는 정보가 담기는 state들 생성
-    const [inputTitle, setInputTitle] = useState("");
-    const [inputContent, setInputContent] = useState("");
+  // 사용자가 input하는 정보가 담기는 state들 생성
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputContent, setInputContent] = useState("");
 
-
-    // 추가하기 버튼 누르면 실행될 함수
-    // 사용자가 input한 정보를 새로운 todo 객체로 만들고, 이를 todoList 배열에 저장
-    const addTodo = () => {
+  // 추가하기 버튼 누르면 실행될 함수
+  // 사용자가 input한 정보를 새로운 todo 객체로 만들고, 이를 todoList 배열에 저장
+  const addTodo = () => {
     const newTodo = {
       // id: todoList.length+1, 중복 이슈 발생
       id: Date.now(),
       title: inputTitle,
       content: inputContent,
-      isDone: false
+      isDone: false,
     };
-    setTodoList([...todoList, newTodo])
-  }
-  
-    return (
+    setTodoList([...todoList, newTodo]);
+  };
+
+  return (
     <>
-    <h3>추가하기</h3>
-    <section className='add-section'>
-        <section className='inputs'>        
-            <p className='input-label-title'>제목</p>
-            <input className='input-title' id='input-title' value = {inputTitle}
-            onChange={(e) => {setInputTitle(e.target.value);}}/>
-            <p className='input-label-content'>내용</p>
-            <input className='input-content' id='input-content' value = {inputContent}
-            onChange={(e) => {setInputContent(e.target.value)}}/>
+      <h3>추가하기</h3>
+      <section className="add-section">
+        <section className="inputs">
+          <p className="input-label-title">제목</p>
+          <input
+            className="input-title"
+            id="input-title"
+            value={inputTitle}
+            onChange={(e) => {
+              setInputTitle(e.target.value);
+            }}
+          />
+          <p className="input-label-content">내용</p>
+          <input
+            className="input-content"
+            id="input-content"
+            value={inputContent}
+            onChange={(e) => {
+              setInputContent(e.target.value);
+            }}
+          />
         </section>
-        <button className='add-button' id='add-button' onClick={addTodo}>추가하기</button>
+        <button className="add-button" id="add-button" onClick={addTodo}>
+          추가하기
+        </button>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default AddSection
+export default AddSection;
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import {useState} from "react";
+import "./App.css";
 
-import AddSection from './AddSection';
-import WorkingSection from './WorkingSection';
-import DoneSection from './DoneSection';
+import AddSection from "./AddSection";
+import WorkingSection from "./WorkingSection";
+import DoneSection from "./DoneSection";
 
 const App = () => {
   // todo 객체들이 저장되는 배열 todoList 생성
@@ -11,12 +11,12 @@ const App = () => {
 
   return (
     <>
-    <h1>My Todo List</h1>
-      <AddSection todoList = {todoList} setTodoList = {setTodoList}></AddSection>
-      <WorkingSection todoList = {todoList} setTodoList = {setTodoList}></WorkingSection>
-      <DoneSection todoList = {todoList} setTodoList = {setTodoList}></DoneSection>
+      <h1>My Todo List</h1>
+      <AddSection todoList={todoList} setTodoList={setTodoList}></AddSection>
+      <WorkingSection todoList={todoList} setTodoList={setTodoList}></WorkingSection>
+      <DoneSection todoList={todoList} setTodoList={setTodoList}></DoneSection>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
